31d83d8caf1967eb5832508c56a008e44087781b
xenosign <xenosign@naver.com>
2023-09-26 14:42:41 +0900

    add

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import styles from "../styles/Home.module.css";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <h1 className={styles.title}>Hello, next world!</h1>
+      <a href="/search">SEARCH</a>
+      <br />
+      <a href="/setting">SETTING</a>
     </>
   )
 }
---
+++ pages/products/[id].js
@@ -0,0 +1,5 @@
+export default function Products() {
+  return (
+    <div>1</div>
+  )
+}
