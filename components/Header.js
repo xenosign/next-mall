03708b790a66ec348e16b71ab8a11b7b4c10613b
kdtTetz <xenosign@naver.com>
2023-10-09 22:01:32 +0900

    404페이지 적용

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import styles from './Header.module.css';
-import Container from './Container';
+import Link from "next/link";
+import styles from "./Header.module.css";
+import Container from "./Container";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Container className={styles.container}>
         <Link className={styles.logo} href="/">
-          codietmall
+          Tetzmall
         </Link>
         <Link className={styles.setting} href="/setting">
           설정
