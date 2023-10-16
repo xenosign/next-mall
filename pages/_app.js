b900c101f813968631247601afddfdb89dc92cc9
xenosign <xenosign@naver.com>
2023-10-16 14:49:53 +0900

    글로벌 폰트 적용

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,27 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/globals.css";
 import Head from "next/head";
+import { Black_Han_Sans } from 'next/font/google';
+
+const blackHanSans = Black_Han_Sans({
+  weight: ['400'],
+  subsets: [],
+});
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Next-Mall</title>
-        {/* 이미지에 따라서 적용 안될 수 있음 */}
         <link rel="icon" href="/favicon.png" />
+        <style>
+          {`
+            html {
+              font-family: ${blackHanSans.style.fontFamily}, sans-serif;
+            }
+        ` }
+        </style>
       </Head>
       <ThemeProvider>
         <Header />
