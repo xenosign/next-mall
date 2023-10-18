91077f43a990acd5f977cab4186ded941134bf39
kdtTetz <xenosign@naver.com>
2023-10-18 14:13:20 +0900

    Update cart.js

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -8,7 +8,7 @@ let cart = [1, 2, 3];
 
 export default function handler(req, res) {
   if (req.method === 'GET') {
-    return res.status(200).json(cart);
+  return res.status(200).json(cart);
   } else if (req.method === 'PUT') {
     cart = ['a', 'b', 'c'];
     return res.status(200).json(cart);
