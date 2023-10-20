13964aa673fd80ae7ead0fde4673e0642dddc679
kdtTetz <xenosign@naver.com>
2023-10-20 20:51:46 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,12 @@ import Header from "@/components/Header";
 import { ThemeProvider } from "@/lib/ThemeContext";
 import "@/styles/globals.css";
 import Head from "next/head";
-import { Black_Han_Sans } from 'next/font/google';
+import { Black_Han_Sans } from "next/font/google";
 
 const blackHanSans = Black_Han_Sans({
-  weight: ['400'],
+  weight: ["400"],
   subsets: [],
 });
-
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,10 +21,9 @@ export default function App({ Component, pageProps }) {
             html {
               font-family: ${blackHanSans.style.fontFamily}, sans-serif;
             }
-        ` }
+        `}
         </style>
       </Head>
-      {/* 책으로 기본 개념 잡을 것 */}
       <ThemeProvider>
         <Header />
         <Container>
