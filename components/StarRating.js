c33817fe54ce9b9659dd48bd81ebc43d525f634d
kdtTetz <xenosign@naver.com>
2023-10-21 13:46:53 +0900

    Update StarRating.js

--- components/StarRating.js
+++ components/StarRating.js
@@ -1,11 +1,6 @@
 const RATINGS = [1, 2, 3, 4, 5];
 
 export default function StarRating({ value = 1 }) {
-  return (
-    <span>
-      {/* 스타일 참고             */}
-      {RATINGS.map((rating) => (value >= rating ? "★" : "✩"))}
-    </span>
-  );
+  return <span>{RATINGS.map((rating) => (value >= rating ? "★" : "✩"))}</span>;
 }
 6;
