d1bd0a6a0318cc9d37a942de37482965d56ad75b
xenosign <xenosign@naver.com>
2023-10-23 15:21:00 +0900

    정적 페이지 생성

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,34 @@ import Header from "@/components/Header";
 import Container from "@/components/Container";
 import Head from "next/head";
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
+// 정적 생성시, props 받아서 내려줌
+// 이름은 getStaticProps 고정
+export async function getStaticProps() {
+  const res = await axios.get("/products");
+  const products = res.data.results;
 
-  async function getProducts() {
-    const res = await axios.get("/products");
-    const nextProducts = res.data.results;
-    setProducts(nextProducts);
+  return {
+    props: {
+      products,
+    }
   }
+}
 
-  useEffect(() => {
-    getProducts();
-  }, []);
+export default function Home() {
+  // 기존에는 랜더가 완료 되고, js 가 실행 되는 시점에서 react hook 으로 데이터를 받아왔지만
+  // 위의 코드로 변경해서 이제는 정적 생성에서부터 데이터를 받아서 처리함
+
+  // const [products, setProducts] = useState([]);
+
+  // async function getProducts() {
+  //   const res = await axios.get("/products");
+  //   const nextProducts = res.data.results;
+  //   setProducts(nextProducts);
+  // }
+
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
 
   return (
     <>
--- pages/items/[id].js
+++ pages/items/[id].js
@@ -4,34 +4,63 @@ import axios from "@/lib/axios";
 import SizeReviewList from "@/components/SizeReviewList";
 import Image from "next/image";
 
+// 어떤 파일을 만들어야 하는지 알려주는 함수
+export async function getStaticPaths() {
+  return {
+    paths: [
+      // 페이지 2개를 정적 생성
+      {
+        params: { id: '1' }
+      },
+      {
+        params: { id: '2' }
+      }
+    ],
+    // 없는 경로에 대한 처리 방법
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const productId = context.params['id'];
+  const res = await axios.get(`/products/${productId}`);
+  const product = res.data;
+
+  return {
+    props: {
+      product
+    }
+  }
+
+}
+
 export default function Products() {
-  const [product, setProduct] = useState();
-  const [sizeReviews, setSizeReviews] = useState([]);
-  const router = useRouter();
-  const { id } = router.query;
+  // const [product, setProduct] = useState();
+  // const [sizeReviews, setSizeReviews] = useState([]);
+  // const router = useRouter();
+  // const { id } = router.query;
 
-  async function getProduct(targetId) {
-    const res = await axios.get(`/products/${targetId}`);
-    const nextProduct = res.data;
-    setProduct(nextProduct);
-  }
+  // async function getProduct(targetId) {
+  //   const res = await axios.get(`/products/${targetId}`);
+  //   const nextProduct = res.data;
+  //   setProduct(nextProduct);
+  // }
 
-  async function getSizeReviews(targetId) {
-    const res = await axios.get(`/size_reviews/?product_id=${targetId}`);
-    const nextSizeReviews = res.data.results ?? [];
-    setSizeReviews(nextSizeReviews);
-  }
+  // async function getSizeReviews(targetId) {
+  //   const res = await axios.get(`/size_reviews/?product_id=${targetId}`);
+  //   const nextSizeReviews = res.data.results ?? [];
+  //   setSizeReviews(nextSizeReviews);
+  // }
 
-  useEffect(() => {
-    if (!id) return;
+  // useEffect(() => {
+  //   if (!id) return;
 
-    getProduct(id);
-    getSizeReviews(id);
+  //   getProduct(id);
+  //   getSizeReviews(id);
 
-  }, [id])
+  // }, [id])
 
   if (!product) return null;
-
 
   return (
     <div>
